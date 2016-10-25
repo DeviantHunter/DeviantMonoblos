0f61e2580128a0df5558f0605974bab6d772ba7a
Kareem Clarke <clarke.kareem3@gmail.com>
2016-10-25 12:23:29 -0400

    GD 205 H.W
    Just Need Scripts

---
+++ GD 205 W7/Assets/Scott.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scott : MonoBehaviour {
+
+	public float scottStrength = 1000f;
+	public AudioClip ExplosionSound;
+	public GameObject Blueprint;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		Ray beam = Camera.main.ScreenPointToRay (Input.mousePosition);
+		Debug.DrawRay (beam.origin, beam.direction * 1000f, Color.red);
+
+		RaycastHit beamHit = new RaycastHit ();
+
+		if (Physics.Raycast (beam, out beamHit, 1000f)) {
+			Debug.Log ("hit?");
+			if (beamHit.rigidbody) {
+				Debug.Log ("has rigidbody");
+				beamHit.rigidbody.AddForce ((Random.insideUnitSphere) * scottStrength);
+				beamHit.collider.gameObject.gameObject.GetComponent<AudioSource> ().PlayOneShot (ExplosionSound);
+			}
+			if (Input.GetMouseButtonDown (1)) {
+				Instantiate (Blueprint, beamHit.point, Quaternion.identity);
+			}
+		}
+}
+}
---
+++ GD 205 W7/Assets/Timer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;//always remember to include this!
+
+public class Timer : MonoBehaviour {
+	
+	private float mytime = 80;
+	public Text Timetext;
+	private bool Timerisactive=true;
+
+	// Use this for initialization
+	void Start () {
+		Timetext = GetComponent<Text>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Timerisactive) {
+			mytime -= Time.deltaTime;
+			Timetext.text = mytime.ToString ("f0");
+			print (mytime);
+			if (mytime <= 0) {
+				mytime = 0;
+				Timerisactive = false;
+				print ("player lost");
+			}
+		}
+	}
+}
